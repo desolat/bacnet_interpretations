12b7cdd3b2e5a34ab5b12eccf42acb718da8c3de
desolat <nuabaranda@web.de>
2020-10-06 13:54:24 +0200

    refactor: catch and log our own exception(s)

--- scraper.py
+++ scraper.py
@@ -24,36 +24,39 @@ print tds
 print lxml.html.tostring(tds[0])
 paras = tds[0].cssselect('p')
 
-for id, para in enumerate(paras):
-    irLinks = para.cssselect('a')
-    if len(irLinks) != 1:
-        continue
-    irLink = irLinks[0]
-    irTitle = irLink.text
-#    data['title'] = irTitle
-    print irTitle
-    titleMatch = re.match('Interpretation (?P<id>[\d-]+) - (?P<date>.+)$', irTitle)
-    if titleMatch is not None:
-        id = titleMatch.group('id')
-        data = {'id' : id}
-        dateStr = titleMatch.group('date')
-        publishDate = datetime.strptime(dateStr, '%B %d, %Y')
-        print publishDate
-        data['publish_date'] = publishDate
-    else:
-        raise Error('Could not match')
-    
-    relDocUrl = urllib.quote(irLink.attrib.get('href'))
-    absDocUrl = ('/'.join((base, relDocUrl)))
-    print absDocUrl
-    data['doc_url'] = absDocUrl
-    pdfData = urllib2.urlopen(absDocUrl).read()
-    xmldata = scraperwiki.pdftoxml(pdfData)
-    print xmldata
+try:
+    for id, para in enumerate(paras):
+        irLinks = para.cssselect('a')
+        if len(irLinks) != 1:
+            continue
+        irLink = irLinks[0]
+        irTitle = irLink.text
+    #    data['title'] = irTitle
+        print irTitle
+        titleMatch = re.match('Interpretation (?P<id>[\d-]+) - (?P<date>.+)$', irTitle)
+        if titleMatch is not None:
+            id = titleMatch.group('id')
+            data = {'id' : id}
+            dateStr = titleMatch.group('date')
+            publishDate = datetime.strptime(dateStr, '%B %d, %Y')
+            print publishDate
+            data['publish_date'] = publishDate
+        else:
+            raise Exception('Could not match')
 
-    italics = para.cssselect('i')
-    summary = italics[0].text
-    data['summary'] = summary
+        relDocUrl = urllib.quote(irLink.attrib.get('href'))
+        absDocUrl = ('/'.join((base, relDocUrl)))
+        print absDocUrl
+        data['doc_url'] = absDocUrl
+        pdfData = urllib2.urlopen(absDocUrl).read()
+        xmldata = scraperwiki.pdftoxml(pdfData)
+        print xmldata
 
-    scraperwiki.sqlite.save(unique_keys=['id'], data=data)
+        italics = para.cssselect('i')
+        summary = italics[0].text
+        data['summary'] = summary
 
+        scraperwiki.sqlite.save(unique_keys=['id'], data=data)
+except Exception as ex:
+    print ex
+    exit 2
